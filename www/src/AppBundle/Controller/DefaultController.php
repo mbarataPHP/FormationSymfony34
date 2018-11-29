6c24f375de78a16bfe7e73aea3184754058bd7ce
mbarataPHP <mathieu@barata.fr>
2018-11-29 11:56:18 +0100

    correction bug

--- www/src/AppBundle/Controller/DefaultController.php
+++ www/src/AppBundle/Controller/DefaultController.php
@@ -19,6 +19,8 @@ class DefaultController extends Controller
     public function indexAction()
     {
 
-        return $this->render('default/index.html.twig', array('name'=>'Célestin'));
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
